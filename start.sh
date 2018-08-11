cf16ba85bfb76b6fbcfb8133fb884e24b044a380
Antoni Sawicki <as@portworx.com>
2018-08-11 02:50:07 -0700

    start script update

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-exec >/var/log/ubuntu-run.log 2>&1
+exec >/var/log/unattended.log 2>&1
 
 # update hostname
-hostname=$(vmware-rpctool "info-get guestinfo.hostname")
-echo "$hostname" > /etc/hostname
-hostname "$hostname"
-#sed -i "s@ubuntu.ubuntu@$fqdn@g" /etc/hosts
-#sed -i "s@ubuntu@$hostname@g" /etc/hosts
+h=$(vmware-rpctool "info-get guestinfo.hostname")
+hostnamectl set-hostname $h
+echo -e "127.0.0.1\t$h" >> /etc/hosts
 
 # update repos
 apt-get -y update
@@ -15,20 +13,7 @@ apt-get -y update
 #apt-get -y autoremove
 #apt-get -y purge
 
-# as stuff
-apt -yq install jq htop atop iotop iftop iperf3 dstat openssh-server docker.io
-mkdir /home/ubuntu/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCQniDpP1+M1N0mUn8Eeg8cAlqK84TjXhe4BF5kD+XNa5iunGT7s8+PPhLb47pDQTPX+s1vhx5BuCysDDUQPeB43hAuUkDYH3OFWOSzYuaNzqpal3mjdz9bNf8Pjb+cIp0CQ2Q/iuDGjtYcHepuVbeD1DWhSGxG0WL8UPqzahLBl5gjPmjG8OUqxBv87PY9BrUsIA7eokCGwpANsA6z1DX+k0sPGfa2k6ZEEhnZf3jwI64Zx87girx+i/2yWfgXzi4ungkO6ufyGe0UfDvIs7M9r7hAoiM+G7OHnOEIKs+Z2JR74o2VKRHp3Smb+76b4anC7gyPopQq5vzDbaxz2wDT askey" >> /home/ubuntu/.ssh/authorized_keys
-chmod 600 /home/ubuntu/.ssh/authorized_keys
-chown -R ubuntu /home/ubuntu
-cd /usr/local/bin; curl https://getmic.ro | bash
-swapoff -a
-sed -i -e '/swap/d' /etc/fstab
-chmod -x /etc/update-motd.d/*
 echo "%sudo ALL=NOPASSWD: ALL" >> /etc/sudoers
 
-chmod 644 $0
-
 echo " DONE; rebooting ... "
-
 reboot
